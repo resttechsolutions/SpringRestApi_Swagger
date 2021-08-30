c803e6668f551051262539ee019a30012e6854a8
Rafael_Estrella <rafaelalfonso82@gmail.com>
2021-08-30 13:23:26 -0400

    Adding login and home pages Updating BasicConfigurationSecurity to using my login page instate of spring one Adding some style from css Adding Docker plugin in the pom.xml

---
+++ src/main/java/com/resttechsolutions/ereservation/vista/controllers/LoginController.java
@@ -0,0 +1,32 @@
+package com.resttechsolutions.ereservation.vista.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("app")
+public class LoginController {
+
+    @GetMapping(value={"/login", "/"})
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView();
+
+        String errorMessage = "Usuario no autorizado, debe autenticarse";
+
+        mav.addObject("errorMessage", errorMessage);
+        mav.setViewName("login");
+
+        return mav;
+    }
+
+    @GetMapping("/home")
+    public ModelAndView home(){
+        ModelAndView mav = new ModelAndView();
+
+        mav.setViewName("home");
+
+        return mav;
+    }
+}
